3d494595f2e39ad5f0bc18abd341b640529d81e2
MurtMagomedov <vipsdagestana@mail.ru>
2023-10-29 23:21:03 +0300

    Было выполнено практическое задание 3 спринта

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var click = 0
+    var today = Date.now
+    var date = DateFormatter()
+    
+    @IBOutlet var label: UITextView!
+    @IBOutlet var buttonRemove: UIButton!
+    @IBOutlet var buttonPlus: UIButton!
+    @IBOutlet var buttonMinus: UIButton!
+    
+    
     override func viewDidLoad() {
+        date.dateFormat = "d MMM y | HH:mm"
+        
+        label.text = "0"
+        
+       
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    @IBAction func remove(_ sender: UIButton) {
+        click = 0
+        label.text = "\(date.string(from: today)): значение сброшено"
+    }
+    @IBAction func minus(_ sender: UIButton) {
+        if click == 0 {
+            label.text = "\(date.string(from: today)): попытка уменьшить значение счётчика ниже 0"
+        } else {
+            click -= 1
+            label.text = "\(date.string(from: today)): значение изменено на -1"
+        }
+    }
+    @IBAction func plus(_ sender: UIButton) {
+        click += 1
+        label.text = "\(date.string(from: today)): значение изменено на +1"
+    }
+    
 }
 
